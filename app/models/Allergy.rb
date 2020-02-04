a0d2c4cda9b25d84c0420fb8660a805395170637
Sylvia <sylviapap1@gmail.com>
2020-02-03 16:16:41 -0800

    Done.

---
+++ app/models/Allergy.rb
@@ -0,0 +1,20 @@
+class Allergy
+    attr_accessor :user, :ingredient
+
+    @@all = []
+
+    def initialize(user, ingredient)
+        @user = user
+        @ingredient = ingredient
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.ingredients
+        self.all.map {|allergen| allergen.ingredient}
+    end
+
+end
